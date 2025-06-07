ea605c900a8dfdbc007f46c8ee899a58c3bc6826
Ankit Vishwakarma <ankitvishwakarma3354@gmail.com>
2025-06-07 16:03:25 +0530

    ENV Config

--- server/index.js
+++ server/index.js
@@ -9,8 +9,14 @@ const PORT = process.env.PORT || 3000
 
 connectDB()
 
+const CorsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+
 app.use(express.json())
-app.use(cors()); 
+app.use(cors(CorsOptions));
 
 
 app.get('/', (req, res) => {
